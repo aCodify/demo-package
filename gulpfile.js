43e41981c640c7c809d2c52102f6a20a9ec69c1a
Jirayu Kanda <jirayu.k@bizidea.co.th>
2015-08-07 17:08:31 +0700

    add file

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var runSequence = require('run-sequence');
+var connect = require('gulp-connect-php');
+var jade = require('gulp-jade');
+var browserSync = require('browser-sync');
+
+
+
+gulp.task('templates', function() {
+  var YOUR_LOCALS = {};
+ 
+  gulp.src('src/templates/*.jade')
+    .pipe(jade({
+      locals: YOUR_LOCALS
+    }))
+    .pipe(gulp.dest('app/'))
+});
+
+gulp.task('connect-sync', function() {
+    connect.server({}, function (){
+        browserSync({
+            open: false,
+            proxy: 'localhost:4321',
+            port: 1234
+        });
+    });
+});
+
+gulp.task('php', function() {
+  return connect.server({
+    port: 4321,
+    base: 'app/',
+    hostname: '0.0.0.0'
+  });
+});
+
+gulp.task('watch', function() {
+  gulp.watch("src/templates/*.jade", ['templates', browserSync.reload]);
+});
+
+gulp.task('runserver', ['php', 'connect-sync'], function() {});
+
+
+gulp.task('default', function() {
+    return runSequence(['templates'] , ['runserver','watch']);  
+});
